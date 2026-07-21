0d8d05b19826dab1ebcd79a00bad0b2ec8b3cdf0
Ygor000 <siqueira2509@gmail.com>
2026-07-20 22:34:21 -0300

    Corrige nome de variável de ambiente

--- config/media.js
+++ config/media.js
@@ -1,7 +1,7 @@
 import { STORAGE_URL } from "./storage";
 
 export const MEDIA = {
-    backgroundVideo: `${STORAGE_URL}/Save%20the%20date/IMG_8110.mp4`
+    backgroundVideo: `${STORAGE_URL}/Save the date/IMG_8110.mp4`
 }
 
 // logo e profile entram depois no objeto MEDIA
